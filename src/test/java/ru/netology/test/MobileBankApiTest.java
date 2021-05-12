fdb5e93ed45e5f3b1020b3407215476fecac4d50
Vera Spiridonova <ver-spiridonova@yandex.ru>
2021-05-12 12:37:11 +0300

    Test failed

---
+++ src/test/java/ru/netology/test/MobileBankApiTest.java
@@ -0,0 +1,27 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class MobileBankApiTest {
+    @Test
+    void shouldReturn() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Фамилия Имя Отчество")// отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Фамилия Имя"));
+
+    }
+
+}
